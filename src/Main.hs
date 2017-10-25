bd9f40ec5003ce6c474251f3322c38edc6cf6d77
Felix Giese <s4fegies@uni-trier.de>
2017-10-25 17:22:28 +0200

    add proper command line argument handling

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,14 @@ module Main(main) where
 import Parser(parseProgram)
 import Interpreter(interpret)
 import Text.Show.Pretty
+import System.Environment
 
-main = do
-    cont <- readFile "test.while"
-    let ast = parseProgram "test.while" cont
-    let s = ppShow ast
-    putStrLn s
-    case ast of
-        Left err -> fail $ show err
-        Right ast -> do
-            let res = interpret ast [1]
-            print res
-    
+main = getArgs >>= parseArgs
+
+parseArgs [] = putStrLn "Usage: whileInterpreter FILENAME [LIST OF INTEGER ARGUMENTS FOR PROGRAM]"
+parseArgs (file:params) = do
+    cont <- readFile file
+    let progargs = map read params
+    case parseProgram file cont of
+        Left err -> print err
+        Right ast -> print $ interpret ast progargs
