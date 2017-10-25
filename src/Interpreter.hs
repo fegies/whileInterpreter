e294b58b91e722844816751236206e6f94952932
Felix Giese <s4fegies@uni-trier.de>
2017-10-25 16:41:21 +0200

    add a working interpreter

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,16 +1,36 @@
 module Interpreter(interpret) where
 
 import WhileAst
-import Data.Map.Lazy as Map
+
+type State = [(Integer,Integer)]
+
+getVal :: State -> Integer -> Integer
+getVal ((a,b):xs) s = if a == s then b else getVal xs s
+
+setVal :: State -> Integer -> Integer -> State 
+setVal ((a,b):xs) s v = if a == s then (a,v):xs else (a,b):setVal xs s v
 
 interpret :: Ast -> [Integer] -> Integer
 interpret ast args =
     let
-        initList = (0,0) : zip [1..] (args ++ repeat 0)
-        initialVars = Map.fromList initList
-        doneState = interpretStep ast initialVars
-    in done
+        initState = (0,0) : zip [1..] (args ++ repeat 0)
+        doneState = interpretStep ast initState
+    in getVal doneState 0
 
-interpretStep :: Ast -> Map.Map Integer Integer -> Map.Map Integer Integer
+varToInt = read . tail
+
+interpretStep :: Ast -> State -> State
 interpretStep (Assign to var op const) state =
-    Map.insert 
+    let vv = getVal state (varToInt var)
+        rv = case op of
+            Plus -> vv + const
+            Minus -> vv - const
+    in setVal state (varToInt to) rv
+interpretStep (Loop var ast) state = 
+    let count = getVal state (varToInt var)
+        loops = foldr1 (.) $ replicate (fromIntegral count) $ interpretStep ast
+    in if count == 0 then state else loops state
+interpretStep a@(While var ast) state =
+    let val = getVal state (varToInt var)
+    in if val == 0 then state else interpretStep a $ interpretStep ast state
+interpretStep (Sequence l r) state = interpretStep r $ interpretStep l state
--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,17 @@
 module Main(main) where
 
 import Parser(parseProgram)
+import Interpreter(interpret)
 import Text.Show.Pretty
 
 main = do
     cont <- readFile "test.while"
-    let s = ppShow $ parseProgram "test.while" cont
+    let ast = parseProgram "test.while" cont
+    let s = ppShow ast
     putStrLn s
+    case ast of
+        Left err -> fail $ show err
+        Right ast -> do
+            let res = interpret ast [1]
+            print res
+    
